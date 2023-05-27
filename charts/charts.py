0954797476a7bca9ba44a0f3e1a701c968b8bc4a
Germán Salina <gersalina28@gmail.com>
2023-05-27 12:16:06 -0300

    Proyectos

--- charts/charts.py
+++ charts/charts.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 
 def generate_pie_chart():
     labels = ['A', 'B', 'C']
-    values = [200, 34, 120]
+    values = [200, 54, 120]
 
     fig, ax = plt.subplots()
     ax.pie(values, labels=labels)
-    plt.savefig('pie.png')
+    plt.savefig('./chart_pie.png')
     plt.close()
